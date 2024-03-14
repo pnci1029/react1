cefcb163c249a84a1b2350e76e90318cd67259bb
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-14 13:42:15 +0900

    24.03.14 simple function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+const reactDescriptions = ['React', 'Java', 'Python']
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
+  return (
+      <header>
+        <h1>This is Header</h1>
+        <p>
+          {description} Component concepts you will need
+          for almost any app you are going to build!
+        </p>
+      </header>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
