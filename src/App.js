c3ae07bf1671ba01eb75eaf2670382bc7566c144
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-14 14:49:06 +0900

    24.03.14 fix logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './data.js';
+import {CORE_CONCEPTS} from "./data";
 const reactDescriptions = ['React', 'Java', 'Python']
 
 function genRandomInt(max) {
@@ -11,6 +13,7 @@ function Header() {
 
   return (
       <header>
+          <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>This is Header</h1>
         <p>
           {description} Component concepts you will need
@@ -20,27 +23,38 @@ function Header() {
   )
 }
 
+function CoreConcept({image, title, description}) {
+    return (
+        <li>
+            <img src={image} alt={title}/>
+            <h3> {title}</h3>
+            <p>{description}</p>
+        </li>
+    );
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div >
+            <Header/>
+            <main>
+                <section id="core-concepts">
+                    <ul>
+                        <CoreConcept
+                            title = {CORE_CONCEPTS[0].title}
+                            description = {CORE_CONCEPTS[0].description}
+                            image = {CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[1]}/>
+                        <CoreConcept {...CORE_CONCEPTS[2]}/>
+                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                    </ul>
+
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
