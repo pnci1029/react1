151e5daded1e84403e1df82a3e114d5b1cac5461
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-14 14:55:36 +0900

    24.03.14 refactoring app.jsx

--- src/App.js
+++ src/App.js
@@ -2,36 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import './data.js';
 import {CORE_CONCEPTS} from "./data";
-const reactDescriptions = ['React', 'Java', 'Python']
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-      <header>
-          <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>This is Header</h1>
-        <p>
-          {description} Component concepts you will need
-          for almost any app you are going to build!
-        </p>
-      </header>
-  )
-}
-
-function CoreConcept({image, title, description}) {
-    return (
-        <li>
-            <img src={image} alt={title}/>
-            <h3> {title}</h3>
-            <p>{description}</p>
-        </li>
-    );
-}
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 
 
 function App() {
