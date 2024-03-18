675440d4ce87cb10f4db5cb5e69bd7e0d0690809
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-18 14:10:15 +0900

    24.03.18 simple using function into prop

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './data.js';
@@ -8,8 +9,14 @@ import TabButton from "./components/TabButton";
 
 
 function App() {
-    function handleSelect() {
-        console.log("hello world selected");
+    const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
+    let tabContent = 'Please click a button';
+
+    function handleSelect(selectedButton) {
+        // selectedButton -> 'components', 'jsx' ...
+        setSelectedTopic(selectedButton);
+        console.log(selectedTopic);
     }
 
     return (
@@ -32,12 +39,12 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton onSelect={handleSelect}>Components</TabButton>
-                        <TabButton onSelect={handleSelect}>JSX</TabButton>
-                        <TabButton onSelect={handleSelect}>Props</TabButton>
-                        <TabButton onSelect={handleSelect}>State</TabButton>
+                        <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
                     </menu>
-                    Dynamic Content
+                    {selectedTopic}
                 </section>
             </main>
         </div>
