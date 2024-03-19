8d97913084898fe15956da62ab1e69a9191e1ea3
chhong <81909140+pnci1029@users.noreply.github.com>
2024-03-19 14:02:38 +0900

    24.03.19 add simple automatic component type

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -31,20 +31,20 @@ export default function Examples() {
     return (
         <Section id="examples" title="Examples">
             <Tabs ButtonContainer="menu"
-                buttons={<>
-                <TabButton isSelected={selectedTopic === 'components'}
-                           onSelect={() => handleSelect('components')}
-                >Components</TabButton>
-                <TabButton isSelected={selectedTopic === 'jsx'}
-                           onSelect={() => handleSelect('jsx')}
-                >JSX</TabButton>
-                <TabButton isSelected={selectedTopic === 'props'}
-                           onSelect={() => handleSelect('props')}
-                >Props</TabButton>
-                <TabButton isSelected={selectedTopic === 'state'}
-                           onSelect={() => handleSelect('state')}
-                >State</TabButton>
-            </>}>
+                  buttons={<>
+                      <TabButton isSelected={selectedTopic === 'components'}
+                                 onSelect={() => handleSelect('components')}
+                      >Components</TabButton>
+                      <TabButton isSelected={selectedTopic === 'jsx'}
+                                 onSelect={() => handleSelect('jsx')}
+                      >JSX</TabButton>
+                      <TabButton isSelected={selectedTopic === 'props'}
+                                 onSelect={() => handleSelect('props')}
+                      >Props</TabButton>
+                      <TabButton isSelected={selectedTopic === 'state'}
+                                 onSelect={() => handleSelect('state')}
+                      >State</TabButton>
+                  </>}>
                 {tabContent}
             </Tabs>
             <menu>
